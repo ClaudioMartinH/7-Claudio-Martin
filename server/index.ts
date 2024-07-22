70b291d9f7d19f65c7090811b3f949287d08d22b
Claudio Martin <klaudito46@gmail.com>
2024-07-22 12:49:11 +0200

    avanzando en api y sockets

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import {Server} from "socket.io"
+import  path from "path";
 
 
 const PORT = 3000;
@@ -8,8 +9,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
+io.on("connection", (socket) => { 
+    console.log("Usuario se ha conectado")
+    
+    socket.on("message", (msg) => {
+        console.log("Recibimos el mensaje: ", msg);
+        io.emit("message", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("Usuario se ha desconectado")
+    });
+})
+
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/client/index.html`)
+    res.sendFile(path.join(__dirname,`../client/index.html`))
 })
 
 server.listen(PORT, () => {
